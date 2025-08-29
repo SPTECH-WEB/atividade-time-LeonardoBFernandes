eb447aa2054a7e25b4be0e87fb1c1afea92f4793
LeonardoBFernandes <leonardo.fernandes@sptech.school>
2025-08-29 14:19:12 -0300

    Subindo atividade final

--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -1,6 +1,6 @@
 package school.sptech;
 
-public class Main {
+public class Main extends Time{
 
     public static void main(String[] args) {
 
--- src/main/java/school/sptech/Time.java
+++ src/main/java/school/sptech/Time.java
@@ -1,5 +1,44 @@
 package school.sptech;
 
 public class Time {
+    String nome;
+    Integer vitorias, derrotas, empates;
 
+    public Time(){
+        vitorias = 0;
+        derrotas = 0;
+        empates = 0;
+    }
+
+    public void registrarVitoria() {
+        vitorias++;
+    }
+
+    public void registrarEmpate() {
+        empates++;
+    }
+
+    public void registrarDerrota() {
+        derrotas++;
+    }
+
+    public Integer getPontos() {
+        return (vitorias * 3) + empates;
+    }
+
+    public Integer getTotalPartidas() {
+        return vitorias + empates + derrotas;
+    }
+
+    public Integer getAproveitamento() {
+        return ((vitorias * 100) / getTotalPartidas());
+    }
+
+    public void compararAproveitamento(Time adversario) {
+        if (getAproveitamento() > adversario.getAproveitamento()){
+            System.out.println("O time 1 tem maior aproveitamento");
+        } else {
+            System.out.println("O time 2 tem maior aproveitamento");
+        }
+    }
 }
